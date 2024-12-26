b11c705d38fd02f37f962a5f259f169e239c8249
Benjamin Mielke <MIELKET.BEN@GMAIL.COM>
2024-12-26 00:46:58 -0600

    Update board_baseball_streamlit.py

--- board_baseball_streamlit.py
+++ board_baseball_streamlit.py
@@ -85,7 +85,7 @@ def dataframe_to_image(df, header_text):
     
     # Load font (default font in case the system does not have arial)
     try:
-        font = ImageFont.truetype("arial.ttf", 16)
+        font = ImageFont.truetype("Arialbd.ttf", 16)
     except IOError:
         font = ImageFont.load_default()
     
